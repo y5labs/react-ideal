cbcc397288bfc40c5a163788a7a4f89fb3af39f6
Thomas Coats <github@voodoolabs.net>
2021-12-14 00:00:06 +1300

    conversion

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// This order is deathly important
-// There is a ctx pass and a pod pass
-// ctx can only use things defined above
-// This order is also how context providers are added
-// So for context (state) you'll want to be imported after any state you rely on. E.g. state relies on hub so hub has to be imported first
 import './lib/hub'
 import './src/index'
 import './lib/router'
