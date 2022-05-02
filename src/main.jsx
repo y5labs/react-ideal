f82e6c1a02d073038b74af699fe143aaa9f5ec6d
Josh Wilson <joshdoesthis@icloud.com>
2022-05-03 11:32:40 +1200

    added time scale filter buttons

--- src/main.jsx
+++ src/main.jsx
@@ -141,106 +141,117 @@ inject('pod', ({ StateContext, HubContext }) => {
         const calc_col_width = useCallback(() => col_width, [renderCount])
         const calc_row_height = useCallback(() => row_height, [renderCount])
 
-        return (
-          <div className='wrapper'>
-            <div className='task-title'>Tasks</div>
-            <TimelimeAxis
-              setScrollOffsetLeft={setScrollOffsetLeft}
-              scrollOffsetLeft={scrollOffsetLeft}
-              size={time_axis_size}
-              calc_col_width={calc_col_width}
-              render={(virtualItems, render) => {
-                const dims = [+Infinity, -Infinity]
-                for (const i of virtualItems) {
-                  dims[0] = Math.min(dims[0], i.index)
-                  dims[1] = Math.max(dims[1], i.index)
-                }
-                const items = time_window(dims)
-                return virtualItems.map(i =>
-                  render(i, items[i.index - dims[0]])
-                )
-              }}
-            />
-            <TaskAxis
-              setScrollOffsetTop={setScrollOffsetTop}
-              scrollOffsetTop={scrollOffsetTop}
-              size={task_axis_size}
-              calc_row_height={calc_row_height}
-              render={(virtualItems, render) => {
-                const dims = [+Infinity, -Infinity]
-                for (const i of virtualItems) {
-                  dims[0] = Math.min(dims[0], i.index)
-                  dims[1] = Math.max(dims[1], i.index)
-                }
-                const items = task_window(dims)
-                return virtualItems.map(i =>
-                  render(i, items[i.index - dims[0]])
-                )
-              }}
-            />
-            <Schedule
-              selectedIndex={selectedIndex}
-              setScrollOffsetTop={setScrollOffsetTop}
-              scrollOffsetTop={scrollOffsetTop}
-              setScrollOffsetLeft={setScrollOffsetLeft}
-              scrollOffsetLeft={scrollOffsetLeft}
-              row_size={task_axis_size}
-              col_size={time_axis_size}
-              calc_row_height={calc_row_height}
-              calc_col_width={calc_col_width}
-              mapPos={(dims)=>{
-                return dims
-              }}
-              onMove={({ task, index, delta }) => {
-                const months = delta[0] / col_width
-                data[index].start_at = task.start_at.plus({ months })
-                data[index].end_at = task.end_at.plus({ months })
-                setRenderCount(state => state + 1)
-              }}
-              onMoveStart={({ task, index, delta }) => {
-                const months = delta[0] / col_width
-                const d = data[index]
-                d.start_at = task.start_at.plus({ months })
-                if (d.end_at < d.start_at) d.end_at = d.start_at
-                setRenderCount(state => state + 1)
-              }}
-              onMoveEnd={({ task, index, delta }) => {
-                const months = delta[0] / col_width
-                const d = data[index]
-                d.end_at = task.end_at.plus({ months })
-                if (d.end_at < d.start_at) d.start_at = d.end_at
-                setRenderCount(state => state + 1)
-              }}
-              onTap={({ task, index }) => {
-                if (selectedIndex == index) setSelectedIndex(null)
-                else setSelectedIndex(index)
-                setRenderCount(state => state + 1)
-              }}
-              render={(row_v, col_v, render) => {
-                const row_dims = [+Infinity, -Infinity]
-                for (const i of row_v) {
-                  row_dims[0] = Math.min(row_dims[0], i.index)
-                  row_dims[1] = Math.max(row_dims[1], i.index)
-                }
-                const col_dims = [+Infinity, -Infinity]
-                for (const i of col_v) {
-                  col_dims[0] = Math.min(col_dims[0], i.index)
-                  col_dims[1] = Math.max(col_dims[1], i.index)
-                }
-                const items = schedule_window(row_dims, col_dims)
+        const [scale, setScale] = useState('1m') // default scale
+        const Timescale = p => {
+          return (
+            <button
+              className={p.scale == p.unit ? 'selected' : ''}
+              onClick={() => p.updateScale(p.unit)}
+            >
+              {p.unit}
+            </button>
+          )
+        }
 
-                return row_v.map(r =>
-                  col_v.map(c =>
-                    render(
-                      r,
-                      c,
-                      items[r.index - row_dims[0]][c.index - col_dims[0]]
-                    )
+        return (
+          <>
+            <div className='filters'>
+              <nav className='timescales'>
+                {['1m', '1q', '1y'].map(u => (
+                  <Timescale scale={scale} updateScale={setScale} unit={u} />
+                ))}
+              </nav>
+            </div>
+            <div className='wrapper'>
+              <div className='task-title'>Tasks</div>
+              <TimelimeAxis
+                setScrollOffsetLeft={setScrollOffsetLeft}
+                scrollOffsetLeft={scrollOffsetLeft}
+                size={time_axis_size}
+                calc_col_width={calc_col_width}
+                render={(virtualItems, render) => {
+                  const dims = [+Infinity, -Infinity]
+                  for (const i of virtualItems) {
+                    dims[0] = Math.min(dims[0], i.index)
+                    dims[1] = Math.max(dims[1], i.index)
+                  }
+                  const items = time_window(dims)
+                  return virtualItems.map(i => render(i, items[i.index - dims[0]]))
+                }}
+              />
+              <TaskAxis
+                setScrollOffsetTop={setScrollOffsetTop}
+                scrollOffsetTop={scrollOffsetTop}
+                size={task_axis_size}
+                calc_row_height={calc_row_height}
+                render={(virtualItems, render) => {
+                  const dims = [+Infinity, -Infinity]
+                  for (const i of virtualItems) {
+                    dims[0] = Math.min(dims[0], i.index)
+                    dims[1] = Math.max(dims[1], i.index)
+                  }
+                  const items = task_window(dims)
+                  return virtualItems.map(i => render(i, items[i.index - dims[0]]))
+                }}
+              />
+              <Schedule
+                selectedIndex={selectedIndex}
+                setScrollOffsetTop={setScrollOffsetTop}
+                scrollOffsetTop={scrollOffsetTop}
+                setScrollOffsetLeft={setScrollOffsetLeft}
+                scrollOffsetLeft={scrollOffsetLeft}
+                row_size={task_axis_size}
+                col_size={time_axis_size}
+                calc_row_height={calc_row_height}
+                calc_col_width={calc_col_width}
+                mapPos={dims => {
+                  return dims
+                }}
+                onMove={({ task, index, delta }) => {
+                  const months = delta[0] / col_width
+                  data[index].start_at = task.start_at.plus({ months })
+                  data[index].end_at = task.end_at.plus({ months })
+                  setRenderCount(state => state + 1)
+                }}
+                onMoveStart={({ task, index, delta }) => {
+                  const months = delta[0] / col_width
+                  const d = data[index]
+                  d.start_at = task.start_at.plus({ months })
+                  if (d.end_at < d.start_at) d.end_at = d.start_at
+                  setRenderCount(state => state + 1)
+                }}
+                onMoveEnd={({ task, index, delta }) => {
+                  const months = delta[0] / col_width
+                  const d = data[index]
+                  d.end_at = task.end_at.plus({ months })
+                  if (d.end_at < d.start_at) d.start_at = d.end_at
+                  setRenderCount(state => state + 1)
+                }}
+                onTap={({ task, index }) => {
+                  if (selectedIndex == index) setSelectedIndex(null)
+                  else setSelectedIndex(index)
+                  setRenderCount(state => state + 1)
+                }}
+                render={(row_v, col_v, render) => {
+                  const row_dims = [+Infinity, -Infinity]
+                  for (const i of row_v) {
+                    row_dims[0] = Math.min(row_dims[0], i.index)
+                    row_dims[1] = Math.max(row_dims[1], i.index)
+                  }
+                  const col_dims = [+Infinity, -Infinity]
+                  for (const i of col_v) {
+                    col_dims[0] = Math.min(col_dims[0], i.index)
+                    col_dims[1] = Math.max(col_dims[1], i.index)
+                  }
+                  const items = schedule_window(row_dims, col_dims)
+
+                  return row_v.map(r =>
+                    col_v.map(c => render(r, c, items[r.index - row_dims[0]][c.index - col_dims[0]]))
                   )
-                )
-              }}
-            />
-          </div>
+                }}
+              />
+            </div>
+          </>
         )
       }
 
