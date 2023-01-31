f0e7d124597bc6ef6bcade99af789d0d6ef20e4b
Josh Wilson <joshdoesthis@icloud.com>
2023-01-31 13:59:00 +1300

    fix

--- lib/react.jsx
+++ lib/react.jsx
@@ -6,13 +6,13 @@ import { BrowserTracing } from '@sentry/tracing'
 const { VITE_SENTRY_SECRET_KEY, VITE_SENTRY_HOST, VITE_SENTRY_PROJECT_ID, VITE_SENTRY_TRACE_SAMPLE_RATE } = import.meta
   .env
 
-if ((VITE_SENTRY_SECRET_KEY, VITE_SENTRY_HOST, VITE_SENTRY_PROJECT_ID))
+if (VITE_SENTRY_SECRET_KEY && VITE_SENTRY_HOST && VITE_SENTRY_PROJECT_ID)
   Sentry.init({
     dsn: `https://${VITE_SENTRY_SECRET_KEY}@${VITE_SENTRY_HOST}/${VITE_SENTRY_PROJECT_ID}`,
     integrations: [new BrowserTracing()],
     tracesSampleRate: VITE_SENTRY_TRACE_SAMPLE_RATE ?? 1
   })
-else console.warn('Sentry is not configured')
+else console.warn('Sentry is not configured.')
 
 inject('pod', async () => {
   const App = inject.one('app')
